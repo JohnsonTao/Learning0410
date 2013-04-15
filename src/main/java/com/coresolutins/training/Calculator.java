57394c67cb6683ed640f56847a44111368f05610
Johnson.Chen <johnson.chen@coresolutions.com>
2013-04-15 13:38:25 +0800

    Learning0415

---
+++ src/main/java/com/coresolutins/training/Calculator.java
@@ -0,0 +1,31 @@
+package com.coresolutins.training;
+
+public class Calculator {
+	private final static int PLUS = 1;
+	private final static int MINUS = 2;
+	private final static int MULTIPLY = 3;
+	private final static int DIVIDE = 4;
+	
+    public int calculate(int opt, int num1, int num2) {
+    	int ret = 0;
+    	switch (opt) {
+		case PLUS:
+			ret = num1 + num2;
+			break;
+		case MINUS:
+			ret = num1 - num2;
+			break;
+		case MULTIPLY:
+			ret = num1 * num2;
+			break;
+		case DIVIDE:
+			ret = num1 / num2;
+			break;			
+		default:
+			break;
+		}
+        return ret;
+    }
+
+  
+}
---
+++ src/main/java/com/coresolutins/training/CalculatorTest.java
@@ -0,0 +1,38 @@
+package com.coresolutins.training;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testGenerateOld() {
+        final Calculator calculator = new Calculator();
+
+        assertTrue(calculator.calculate(1, 3, 8) == 11 && calculator.calculate(2, 3, 1) == 2
+                && calculator.calculate(3, 3, 2) == 6 && calculator.calculate(4, 15, 2) == 7
+                && calculator.calculate(5, 6, 2) == 0);
+    }
+
+}
